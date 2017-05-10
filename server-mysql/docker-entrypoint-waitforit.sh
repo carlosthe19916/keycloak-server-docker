40fefea7da392c7c155168ee6b75ae52dd6d8c71
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-05-10 16:42:24 -0500

    changes

--- server-mysql/docker-entrypoint-waitforit.sh
+++ server-mysql/docker-entrypoint-waitforit.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/opt/jboss/wait-for-it.sh ${MYSQL_PORT_3306_TCP_ADDR:-mysql}:${MYSQL_PORT_3306_TCP_PORT:-3306} -t 60 $@
+exec /opt/jboss/wait-for-it.sh ${MYSQL_PORT_3306_TCP_ADDR:-mysql}:${MYSQL_PORT_3306_TCP_PORT:-3306} -t 60 
+exec /opt/jboss/wildfly/bin/standalone.sh $@
 exit $?
